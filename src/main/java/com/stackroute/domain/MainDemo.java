8f897cb1ab202a68e1b1fca44a2c722c85b924e5
shprdn <51949916+shprdn@users.noreply.github.com>
2019-07-19 10:24:48 +0530

    committed

--- src/main/java/com/stackroute/domain/MainDemo.java
+++ src/main/java/com/stackroute/domain/MainDemo.java
@@ -9,24 +9,25 @@ public class MainDemo {
     public static void main(String[] args) {
         //setting the configuration location
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-        //creating actor class objects
-        Actor actor1 = context.getBean("actor1",Actor.class);
-        System.out.println(actor1);
-        Actor actor2 = context.getBean("actor2",Actor.class);
-        System.out.println(actor2);
-        Actor actor3 = context.getBean("actor3",Actor.class);
-        System.out.println(actor3);
-       //creating movie class objects
-        Movie movie1 = context.getBean("movie1", Movie.class);
-        //movie1.getDetails();
-        Movie movie2 = context.getBean("movie1",Movie.class);
-       // movie2.getDetails();
-        System.out.println(movie1==movie2);
-        //for the beans with name not id
-        Movie movieA = context.getBean("movieA",Movie.class);
-        movieA.getDetails();
-        Movie movieB = context.getBean("movieB", Movie.class);
-        movieB.getDetails();
+        //creating movie class objects
+        Movie movie1 = context.getBean("movie", Movie.class);
+        movie1.getDetails();
+        
+        //using XmlBeanFactory
+        Resource resource = new ClassPathResource("beans.xml");
+        //creating movie class objects
+        BeanFactory beanfactory = new XmlBeanFactory(resource);
+        Movie movie2 = beanfactory.getBean("movie", Movie.class);
+        movie2.getDetails();
+        
+        //using BeanDefinitionReader and BeanDefinitionRegistry
+        BeanDefinitionRegistry bandefinitionregistry = new DefaultListableBeanFactory("beans.xml");
+        BeanDefinitionReader beandefinitionreader = new XmlBeanDefinitionReader(beanDefinitionRegistry);
+        beandefinitionreader.loadBeanDefinitionReader(new ClassPathResource("beans.xml"));
+        //creating movie class objects
+        Movie movie3 = (Movie) ((DefaultListableBeanFactory) beandefinitionregistry).getBean("movie");
+        movie3.getDetails();
+        
 
 
     }
